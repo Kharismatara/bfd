bf41be99ece0c788984c4e8abfe55b9df5e747dc
kharisma tara mukti <kharismataram@gmail.com>
2022-09-03 22:02:55 +0700

    index

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FrontendController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,13 +21,38 @@ Route::get('/details/{slug}', [FrontendController::class, 'details'])->name('det
 Route::get('/cart', [FrontendController::class, 'cart'])->name('cart');
 Route::get('/checkout/success', [FrontendController::class, 'success'])->name('checkout-success');
 
-Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->name('dashboard')->group(function(){
-    Route::get('/',[DashboardController::class, 'index'])->name('index');//dashboard.index
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+//     Route::middleware(['admin'])->group(function () {
+//     Route::resource('product', ProductController::class);
+        
+//     });
 
+// }
+// );
+Route::name('dashboard.')->prefix('dashboard')->middleware('admin')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('index');
 
-    Route::middleware(['admin'])->group(function(){
-
+    Route::middleware(['admin'])->group(function () {
+        Route::resource('product', ProductController::class);
+       
     });
 });
+
+
+
+// Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->name('dashboard')->prefix('dashboard')->group(function () {
+//     Route::get('/', [DashboardController::class,'index'])->name('index');
+
+// }
+// );
+
+
 
 
